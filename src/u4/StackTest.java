6bd15d4c0a90f3082b01afd26324598ff1991a75
Marco Koch <marco@5th.ch>
2014-10-17 18:52:52 +0200

    new test

--- src/u4/StackTest.java
+++ src/u4/StackTest.java
@@ -4,40 +4,52 @@ public class StackTest {
 
 	public static void main(String[] args) {
 		Stack myStack = new Stack(5);
-		System.out.println(myStack.size());
+		System.out.println("size: " + myStack.size());
 		
 		myStack.push(5);
 		myStack.push("hello");
 		myStack.push(6.7);
 		
-		System.out.println(myStack.size());
+		System.out.println("size: " + myStack.size());
 		
 		myStack.push(42);
 		myStack.push("oneMore");
 		
-		System.out.println(myStack.size());
+		System.out.println("size: " + myStack.size());
 		
 		myStack.push("oneTooMany");
 		
-		System.out.println(myStack.pop());
-		System.out.println(myStack.pop());
-		System.out.println(myStack.pop());
+		System.out.println("pop: " + myStack.pop());
+		System.out.println("pop: " + myStack.pop());
+		System.out.println("pop: " + myStack.pop());
 		
 		myStack.push(myStack);
 		
-		System.out.println(myStack.pop());
-		System.out.println(myStack.pop());
-		System.out.println(myStack.pop());
-		System.out.println(myStack.pop());
+		System.out.println("pop: " + myStack.pop());
+		System.out.println("pop: " + myStack.pop());
+		System.out.println("pop: " + myStack.pop());
+		System.out.println("pop: " + myStack.pop());
 		
+		System.out.println("-----");
+
 		Stack myOtherStack = new Stack(2);
 		myOtherStack.push("foo");
 		
-		System.out.println(myOtherStack.peek());
+		System.out.println("peek: " + myOtherStack.peek());
 		myOtherStack.push("bar");
-		System.out.println(myOtherStack.size());
-		System.out.println(myOtherStack.pop());
-		System.out.println(myOtherStack.pop());
+		System.out.println("size: " + myOtherStack.size());
+		System.out.println("pop: " + myOtherStack.pop());
+		System.out.println("pop: " + myOtherStack.pop());
+
+		System.out.println("-----");
+
+		Stack s = new Stack(10);
+		Stack k = new Stack(10);
+
+		k.push(3);
+		k.pop();
+
+		System.out.println(s.size() + " " + k.size());
 		
 	}
 }
