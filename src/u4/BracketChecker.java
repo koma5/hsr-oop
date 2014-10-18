feec093890713e8c3124d32f6b4e0d85729032a8
Marco Koch <marco@5th.ch>
2014-10-18 12:35:04 +0200

    added miserable but working BracketChecker excercise

---
+++ src/u4/BracketChecker.java
@@ -0,0 +1,58 @@
+package u4;
+
+public class BracketChecker {
+	
+	private String inputString;
+	private Stack openedBrackets = new Stack(100);
+	private final String[] brackets = {
+			"{[(<",
+			"}])>"
+	};
+	private int position;
+	
+	public boolean validate() {
+
+		if (position < inputString.length()) {
+			//System.out.println(string.charAt(pos));
+			if (isOpeningBracket(inputString.charAt(position))) {
+				openedBrackets.push(inputString.charAt(position));
+				position++;
+				return validate();
+			}
+			else {
+				int closingBracketType = brackets[1].indexOf(inputString.charAt(position));
+				int openingBracketType = brackets[0].indexOf((char)openedBrackets.pop());
+				
+				if (closingBracketType == openingBracketType) {
+					position++;
+					return true && validate();
+				}
+				else {
+					return false;
+				}
+			}
+		}
+		else if (openedBrackets.peek() == null) {
+				return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public boolean validate(String input) {
+		this.inputString = input;
+		position = 0;
+		return validate();
+	}
+	
+	private boolean isOpeningBracket(char c) {
+		if (brackets[0].indexOf(c) > -1 ) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+}
---
+++ src/u4/BracketCheckerTest.java
@@ -0,0 +1,19 @@
+package u4;
+
+public class BracketCheckerTest {
+	public static void main(String[] args) {
+		bracketTest("Test1", "(<[()]>){}", true);
+		bracketTest("Test2", "()[]{}<>", true);
+		bracketTest("Test3", "(((<>)))([<>])", true);
+		bracketTest("TestBlank", "", true);
+		bracketTest("Test4", "<", false);
+		bracketTest("Test5", "[()>", false);
+		bracketTest("Test6", "{)(][}", false);
+		bracketTest("Test7", "({)}", false);
+	}
+
+	private static void bracketTest(String testName, String input, boolean expected) {
+		boolean result = new BracketChecker().validate(input);
+		System.out.println(testName + ": " + (expected == result ? "ok" : "failed"));
+	}
+}
