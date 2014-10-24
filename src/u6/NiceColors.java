b3e1a60f024d04fcc620d9cbfde232f35b79d595
Marco Koch <marco@5th.ch>
2014-10-24 11:19:22 +0200

    added class NiceColors

---
+++ src/u6/NiceColors.java
@@ -0,0 +1,11 @@
+package u6;
+
+public final class NiceColors {    // nicht veränderbar - nicht vererbbar - aber instanzierbar
+	public static final int BLUE   = 0x1f497d;
+	public static final int RED    = 0xc0504d;
+	public static final int GREEN  = 0x9bbb59;
+	public static final int ORANGE = 0xf79646;
+	public static final int WHITE  = 0xffffff;
+	public static final int BLACK  = 0x000000;
+	
+}
